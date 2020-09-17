c3dfce22fa9b3c1f304422bab13a50b954ee4e07
Anna Alexandra Nery <annaalexandra.nery@gmail.com>
2020-09-17 19:50:02 +0100

    My First Commit

---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="main_container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
